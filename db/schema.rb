d0a94fbf9d6823f0beb421acef8cb3140e92fa95
George Pasley <gpasley@3freed.com>
2013-05-12 23:04:16 -0400

    heroku time

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130512151935) do
+ActiveRecord::Schema.define(:version => 20130513025354) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20130512151935) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "username"
+    t.string   "first_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
